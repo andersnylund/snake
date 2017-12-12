49ba5dda6b72f7b39afb6bd2a210a2e8800b6009
Anders Nylund <anders.nylund.an@gmail.com>
2017-12-12 10:55:26 +0200

    Add reckless snake

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
+  iframe = document.getElementById('iframe');
   canvas = document.getElementById('canvas');
   context = canvas.getContext('2d');
-  snake = new Snake(10, 10);
-  snake.draw();
+  snake = new Snake();
+  start();
 });
+
 
 /**
  * Global variables
  */
+var iframe;
 var canvas;
 var context;
 var snake;
+var size = 10; // the size of one rectangle
+
 
 /**
  * Clear everything from the canvas 
@@ -19,56 +24,116 @@ function clearCanvas() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+
 /**
- * Snake class
+ * Direction enum
+ */
+const Direction = Object.freeze({
+  left: 1,
+  up: 2,
+  right: 3,
+  down: 4
+});
+
+
+/**
+ * Snake 
  */
 class Snake {
+  constructor() {
+    this.direction = Direction.right;
+    this.rects = [];
+    for (var i = 5; i > 0; --i) {
+      this.rects.push(new Position(i * size, size));
+    }
+  }
+}
+
+/**
+ * Position of one rectangle
+ */
+class Position {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.size = 10;
+  }
+}
+
+
+
+function moveSnake() {
+
+  var newHead = snake.rects.pop();
+
+  switch (snake.direction) {
+    case 1: // left
+      newHead.x = snake.rects[0].x - size;
+      newHead.y = snake.rects[0].y;
+      break;
+
+    case 2: // up
+      newHead.x = snake.rects[0].x;
+      newHead.y = snake.rects[0].y - size;
+      break;
+
+    case 3: // right
+      newHead.x = snake.rects[0].x + size;
+      newHead.y = snake.rects[0].y;
+      break;
+
+    case 4: // down
+      newHead.x = snake.rects[0].x;
+      newHead.y = snake.rects[0].y + size;
+      break;
   }
 
-  draw() {
-    clearCanvas(); // clear the canvas
-    context.fillRect(this.x, this.y, this.size, this.size);
-  }
+  snake.rects.unshift(newHead);
 
-  move(x, y) {
-    this.x = this.x + x;
-    this.y = this.y + y;
-  }
+  drawSnake();
+}
+
+function drawSnake() {
+  clearCanvas(); // clear the canvas completely, not efficient at all
+  snake.rects.forEach(rect => {
+    context.fillRect(rect.x, rect.y, size, size);
+  });
 }
 
 
 /**
  * Register key presses
  */
-$(document).keydown(function(e) {
-  switch(e.which) {
-      case 37: // left
-        snake.move(-snake.size, 0);
-        snake.draw();
+$(document).keydown(function (e) {
+  switch (e.which) {
+    case 37: // left
+      if (snake.direction === Direction.right) break;
+      snake.direction = Direction.left;
       break;
 
-      case 38: // up
-        snake.move(0, -snake.size);
-        snake.draw();
+    case 38: // up
+      if (snake.direction === Direction.down) break;
+      snake.direction = Direction.up;
       break;
 
-      case 39: // right
-        snake.move(snake.size, 0);
-        snake.draw();
+    case 39: // right
+      if (snake.direction === Direction.left) break;
+      snake.direction = Direction.right;
       break;
 
-      case 40: // down
-        snake.move(0, snake.size);
-        snake.draw();
+    case 40: // down
+      if (snake.direction === Direction.up) break;
+      snake.direction = Direction.down;
       break;
 
-      default: return; // exit this handler for other keys
+    default: return; // exit this handler for other keys
   }
   e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
 
+/**
+ * Start 
+ */
+function start() {
+  var interval = window.setInterval(moveSnake, 80);
+}
