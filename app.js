f5f6bccd4291d1838b0d83d8623d17f4759a24f7
Anders Nylund <anders.nylund.an@gmail.com>
2018-02-03 17:36:07 +0200

    Use node to start express server

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-var https
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', function requestHandler(request, response){
+  response.sendFile(path.join(__dirname+'/snake.html'))
+});
+
+app.get('/snake.js', function requestHandler(request, response){
+  response.sendFile(path.join(__dirname+'/snake.js'))
+});
+
+app.get('/style.css', function requestHandler(request, response){
+  response.sendFile(path.join(__dirname+'/style.css'))
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${ PORT }`))
